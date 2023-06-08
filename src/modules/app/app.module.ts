6412078fcf3c6c8e95ae9b1c5cecb51e64190cf0
Andrew_Yatsuk <andrii.yatsuk.pz.2020@gmail.com>
2023-06-08 15:45:46 +0300

    db connection fix

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { configuration } from 'src/config/configuration';
 import { validationSchema } from 'src/config/validation';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { get_db_config } from 'src/config/database.config';
 
 @Module({
   imports: [
@@ -14,18 +14,7 @@ import { AppService } from './app.service';
       load: [configuration],
       validationSchema,
     }),
-    TypeOrmModule.forRootAsync({
-      extraProviders: [ConfigModule],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get('postgres_host'),
-        port: configService.get('postgres_port'),
-        username: configService.get('postgres_user'),
-        database: configService.get('postgres_db'),
-        entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      }),
-      inject: [ConfigService],
-    }),
+    TypeOrmModule.forRootAsync(get_db_config()),
   ],
   controllers: [AppController],
   providers: [AppService],
