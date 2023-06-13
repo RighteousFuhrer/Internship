5039f16b8ad281563b1792d9834b3db64bbd92a7
Andrew_Yatsuk <andrii.yatsuk.pz.2020@gmail.com>
2023-06-14 00:08:32 +0300

    Revert to just task1

--- src/config/interfaces.ts
+++ src/config/interfaces.ts
@@ -1,21 +1,3 @@
-export interface IAppConfig extends IServerConfig {
-  database: IDbConfig;
-}
-
 export interface IServerConfig {
   port: number;
-  jwt_rt_secret: string;
-  jwt_at_secret: string;
-}
-
-export interface IDbConfig {
-  type: string;
-  host: string;
-  port: number;
-  username: string;
-  password: string;
-  database: string;
-  name: string;
-  entities: string[];
-  migrations: string[];
 }
--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -2,16 +2,14 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import appConfig from '../../config/app.config';
-import dbConfig from '../../config/database.config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthModule } from '../auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      load: [appConfig, dbConfig],
+      load: [appConfig],
     }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
@@ -20,7 +18,6 @@ import { AuthModule } from '../auth/auth.module';
       }),
       inject: [ConfigService],
     }),
-    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
