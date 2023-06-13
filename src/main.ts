27cb495e66f8318e71fccf9614a7afc97e6c0ca8
Andrew_Yatsuk <andrii.yatsuk.pz.2020@gmail.com>
2023-06-14 00:08:32 +0300

    Revert to just task1

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,13 @@ import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { version, name, description } from 'package.json';
 import { AppModule } from './modules/app/app.module';
-import { Logger, ValidationPipe } from '@nestjs/common';
+import { Logger } from '@nestjs/common';
 
-import type { IAppConfig } from './config/interfaces';
+import type { IServerConfig } from './config/interfaces';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
-  const configService = app.get<ConfigService<IAppConfig, true>>(ConfigService);
+  const configService = app.get<ConfigService<IServerConfig, true>>(ConfigService);
 
   const swaggerConfig = new DocumentBuilder()
     .setTitle(name)
@@ -18,10 +18,6 @@ async function bootstrap(): Promise<void> {
     .build();
   const document = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('api', app, document);
-
-  app.useGlobalPipes(
-    new ValidationPipe(),
-  );
 
   // * for postman debuging
   // should be replaced with localhost:3000 in final version
