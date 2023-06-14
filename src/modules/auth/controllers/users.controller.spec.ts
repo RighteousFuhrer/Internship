bfaa54ffc20e4d5f2f612264b38f8ec58edea7e0
Andrew_Yatsuk <andrii.yatsuk.pz.2020@gmail.com>
2023-06-14 13:04:51 +0300

    user tests

--- src/modules/auth/controllers/users.controller.spec.ts
+++ src/modules/auth/controllers/users.controller.spec.ts
@@ -1,15 +1,51 @@
 import { Test } from '@nestjs/testing';
 import { UsersController } from './users.controller';
-
+import { UsersService } from '../services/users.service';
 import type { TestingModule } from '@nestjs/testing';
+import { BadRequestException, NotFoundException } from '@nestjs/common';
+
+import type { UpdateUserDto } from '../dtos/update.user.dto';
 
 describe('UsersController', () => {
+
   let controller: UsersController;
+
+  const mockUser = {
+    id:1,
+    email: 'example@mail',
+    first_name: 'John',
+    last_name: 'Doe',
+    password: 'password',
+    image: Buffer.from('', 'base64'),
+    hashedRt: '',
+  };
+
+  const mockUsersService = {
+    findOneById: jest.fn( (id:number) => {
+      if(id === mockUser.id) {
+        return mockUser;
+      }
+      return null;
+    }),
+    updateUser: jest.fn( (id:number, dto:UpdateUserDto) => {
+      if(id !== mockUser.id) {
+        throw new NotFoundException('User not found');
+      }
+
+      return { ...mockUser, ...dto };
+    }),
+    deleteUser : (id : number) : void => {
+      if(id !== mockUser.id) {
+        throw new BadRequestException('Failed to delete');
+      }
+    },
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
-    }).compile();
+      providers: [UsersService],
+    }).overrideProvider(UsersService).useValue(mockUsersService).compile();
 
     controller = module.get<UsersController>(UsersController);
   });
@@ -17,4 +53,41 @@ describe('UsersController', () => {
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+  it('should return a user', () : void => {
+    expect(controller.findOne(1)).resolves.toBe(mockUser);
+  });
+
+  it('should update a user', () : void => {
+    const dto = {
+      email: 'new@mail.com',
+      first_name: 'New first',
+      last_name: 'New last',
+      password: 'new pass',
+      image: Buffer.from('', 'base64'),
+    };
+    const id = 1;
+
+    expect(controller.update(dto, id)).resolves.toEqual({ ...mockUser, ...dto });
+  });
+
+  it('should delete a user', () : void => {
+    expect(controller.delete(1)).resolves.toEqual(undefined);
+  });
+
+  it('should throw an exception', () : void => {
+    expect(controller.findOne(2)).rejects.toEqual(new NotFoundException());
+  });
+
+  it('should throw an exception', () : void => {
+    const dto = {
+      email: 'new@mail.com',
+      first_name: 'New first',
+      last_name: 'New last',
+      password: 'new pass',
+      image: Buffer.from('', 'base64'),
+    };
+    expect(controller.update(dto,2)).rejects.toEqual(new NotFoundException('User not found'));
+  });
+
 });
--- src/modules/auth/services/users.service.spec.ts
+++ src/modules/auth/services/users.service.spec.ts
@@ -1,14 +1,70 @@
 import { Test } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { User } from '../entities/user.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import * as bcrypt from 'bcrypt';
 
-import type { TestingModule } from '@nestjs/testing';
+import type { UpdateUserDto } from '../dtos/update.user.dto';
+import type { CreateUserDto } from '../dtos/createUser.dto';
+import { BadRequestException, ForbiddenException, NotFoundException } from '@nestjs/common';
+type findSchema = {
+  where: {
+    email?: string;
+    id?: number;
+  };
+};
 
 describe('UsersService', () => {
   let service: UsersService;
 
+  const mockUser = {
+    id: 1,
+    email: 'example@mail.com',
+    first_name: 'John',
+    last_name: 'Doe',
+    password: bcrypt.hashSync('password', 10),
+    image: Buffer.from('', 'base64'),
+    hashedRt: '',
+  };
+
+  const mockRepository = {
+    findOne: jest.fn(async ({ where }: findSchema) => {
+      if (where.id === mockUser.id || where.email === mockUser.email) {
+        return await mockUser;
+      }
+
+      return null;
+    }),
+    create: jest.fn(async (dto: CreateUserDto) => {
+      return await {
+        ...dto,
+        id: 2,
+        hashedRt: null,
+      };
+    }),
+    save: jest.fn(async (user: User) => {
+      return await user;
+    }),
+    update: jest.fn(async (id: number, updateUserDto: UpdateUserDto) => {
+      if (updateUserDto && id !== mockUser.id) {
+        return await { affected: 0 };
+      }
+      return await { affected: 1 };
+    }),
+    delete: jest.fn(async (id: number) => {
+      if (id !== mockUser.id) {
+        return await { affected: 0 };
+      }
+      return await { affected: 1 };
+    }),
+  };
+
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+    const module = await Test.createTestingModule({
+      providers: [
+        UsersService,
+        { provide: getRepositoryToken(User), useValue: mockRepository },
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
@@ -16,5 +72,94 @@ describe('UsersService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should return a user', () => {
+    expect(service.findOneByEmail('example@mail.com')).resolves.toEqual(
+      mockUser,
+    );
+  });
+  it('should return a user', () => {
+    expect(service.findOneById(1)).resolves.toEqual(mockUser);
+  });
+  it('should create a user', () => {
+    const dto = {
+      email: 'example@mail.com',
+      password: bcrypt.hashSync('password', 10),
+      first_name: 'John',
+      last_name: 'Doe',
+      image: Buffer.from('', 'base64'),
+    };
+    expect(service.createUser(dto)).resolves.toEqual({
+      ...dto,
+      id: 2,
+      hashedRt: null,
+    });
+  });
+  it('should return null', () => {
+    expect(service.findOneByEmail('')).resolves.toEqual(
+      null,
+    );
+  });
+  it('should return null', () => {
+    expect(service.findOneById(3)).resolves.toEqual(
+      null,
+    );
+  });
+  it('should update a user', () => {
+    const id = 1;
+    const dto = {
+      email: 'example@mail.com',
+      password: bcrypt.hashSync('password', 10),
+      first_name: 'John',
+      last_name: 'Doe',
+      image: Buffer.from('', 'base64'),
+    };
+    expect(service.updateUser(id, dto)).resolves.toEqual(mockUser);
+  });
+
+  it('should delete a user', () => {
+    const id = 1;
+    expect(service.deleteUser(id)).resolves.toEqual(undefined);
+  });
+
+  it('should validate a user', () => {
+    const dto = {
+      email: 'example@mail.com',
+      password: 'password',
+    };
+    expect(service.validateUser(dto)).resolves.toEqual(mockUser);
+  });
+
+  it('should throw an exception', (): void => {
+    const dto = {
+      email: 'example@mail.com',
+      password: bcrypt.hashSync('password', 10),
+      first_name: 'John',
+      last_name: 'Doe',
+      image: Buffer.from('', 'base64'),
+    };
+    const id = 2;
+    expect(service.updateUser(id,dto)).rejects.toEqual(new BadRequestException('Failed to update'));
+  });
+
+  it('should throw an exception', (): void => {
+    const id = 2;
+    expect(service.deleteUser(id)).rejects.toEqual(new BadRequestException('Failed to delete'));
+  });
+  it('should throw an exception', (): void => {
+    const dto = {
+      email: 'example@mail',
+      password: 'password',
+    };
+    expect(service.validateUser(dto)).rejects.toEqual(new  NotFoundException('Email not found'));
+  });
+
+  it('should throw an exception', (): void => {
+    const dto = {
+      email: 'example@mail.com',
+      password: 'passwo',
+    };
+    expect(service.validateUser(dto)).rejects.toEqual(new ForbiddenException('Invalid password'));
   });
 });
